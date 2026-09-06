92cd47eb5f1dc82bc3ed5d711f610f706405e419
Alle-0 <alle0.github@example.com>
2026-09-06 16:11:55 +0200

    Compress screenshots to WebP format

--- fix_paths.ps1
+++
@@ -1,9 +0,0 @@
-$htmlFiles = Get-ChildItem -Path .\docs\*.html
-foreach ($file in $htmlFiles) {
-    (Get-Content $file.FullName) -replace 'src="/assets/', 'src="./assets/' -replace 'href="/assets/', 'href="./assets/' | Set-Content $file.FullName
-}
-
-$cssFiles = Get-ChildItem -Path .\docs\*.css
-foreach ($file in $cssFiles) {
-    (Get-Content $file.FullName) -replace 'url\("/assets/', 'url("./assets/' -replace 'url\(''/assets/', 'url(''./assets/' -replace 'url\(/assets/', 'url(./assets/' | Set-Content $file.FullName
-}
